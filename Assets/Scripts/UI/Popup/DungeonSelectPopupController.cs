b18c46c88f7503c1c1af35f71fdccd8e39eb4706
kkj01162 <kkj01162@naver.com>
2023-10-17 01:15:59 +0900

    [mod] 팝업 텍스트 적용 및 Font 한글 깨짐 수정.

--- Assets/Scripts/UI/Popup/DungeonSelectPopupController.cs
+++ Assets/Scripts/UI/Popup/DungeonSelectPopupController.cs
@@ -9,20 +9,39 @@ using HSMLibrary.Extensions;
 using Cysharp.Threading.Tasks;
 using HSMLibrary.Manager;
 using HSMLibrary.Scene;
+using TMPro;
 
 public class DungeonSelectPopupController : UIBaseController , IPopup
 {
     [SerializeField] Button infinityBtn = null;
     [SerializeField] Button timeAttackBtn = null;
     [SerializeField] Button closeBtn = null;
+    [SerializeField] TextMeshProUGUI modeSelectText = null;
 
+    private TextMeshProUGUI infinityText = null;
+    private TextMeshProUGUI timeAttackText = null;
     private UIManager uiMgr = null;
+
+    private const string MODE_SELECT_TEXT = "모드선택";
+    private const string INFINITY_TEXT = "무한모드";
+    private const string TIMEATTACK_TEXT = "타임어택";
     protected override void Awake()
     {
         base.Awake();
         uiMgr = UIManager.getInstance;
         timeAttackBtn.onClick.AddListener(OnClickTimeAttackButton);
         closeBtn.onClick.AddListener(OnClickCloseButton);
+        infinityText = infinityBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        timeAttackText = timeAttackBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Initialize();
+    }
+
+    protected override void Initialize()
+    {
+        //base.Initialize();
+        modeSelectText.text = MODE_SELECT_TEXT;
+        infinityText.text = INFINITY_TEXT;
+        timeAttackText.text = TIMEATTACK_TEXT;
     }
 
     public T Show<T>() where T : IPopup
--- Assets/Scripts/UI/Popup/WeaponSelectPopupController.cs
+++ Assets/Scripts/UI/Popup/WeaponSelectPopupController.cs
@@ -8,14 +8,25 @@ using System;
 using HSMLibrary.Extensions;
 using Cysharp.Threading.Tasks;
 using HSMLibrary.Manager;
+using TMPro;
 
 public class WeaponSelectPopupController : UIBaseController, IPopup
 {
     [SerializeField] Button equipBtn = null;
     [SerializeField] Button joinBtn = null;
     [SerializeField] Button closeBtn = null;
+    [SerializeField] TextMeshProUGUI dungeonJoinText = null;
+    [SerializeField] TextMeshProUGUI equipText = null;
+    [SerializeField] TextMeshProUGUI enhanceText = null;
 
+    private TextMeshProUGUI equipmentText = null;
+    private TextMeshProUGUI joinText = null;
     private UIManager uiMgr = null;
+
+    private const string DUNGEON_JOIN_TEXT = "던전입장";
+    private const string EQUIP_TEXT = "착용중";
+    private const string EQUIPMENT_TEXT = "착용하기";
+    private const string JOIN_TEXT = "입장하기";
 
     protected override void Awake()
     {
@@ -23,6 +34,17 @@ public class WeaponSelectPopupController : UIBaseController, IPopup
         uiMgr = UIManager.getInstance;
         joinBtn.onClick.AddListener(OnClickJoinButton);
         closeBtn.onClick.AddListener(OnClickCloseButton);
+        equipmentText = equipBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        joinText = joinBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Initialize();
+    }
+
+    protected override void Initialize()
+    {
+        dungeonJoinText.text = DUNGEON_JOIN_TEXT;
+        equipText.text = EQUIP_TEXT;
+        equipmentText.text = EQUIPMENT_TEXT;
+        joinText.text = JOIN_TEXT;
     }
 
     public T Show<T>() where T : IPopup
