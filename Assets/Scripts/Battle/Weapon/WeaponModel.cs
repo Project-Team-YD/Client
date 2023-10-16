fcca7f955da69f728788f10b26c4b6f7cb8ce9f9
kkj01162 <kkj01162@naver.com>
2023-10-16 11:20:59 +0900

    [mod]공격 로직 예외 처리 추가 및 IntroScene 터치스크린 문구 추가.

--- Assets/Scripts/Battle/Weapon/WeaponModel.cs
+++ Assets/Scripts/Battle/Weapon/WeaponModel.cs
@@ -61,67 +61,69 @@ public class WeaponSlot : MonoBehaviour
     // TODO :: 원거리 무기는 Bullet같은 스크립트를 하나 파서 거기서 컨트롤해줘야..........생각해보기
     public async void WeaponAttack(WeaponType _type, Transform _transform)
     {
-        coolTime = 0;
-        switch (_type)
+        if (transform != null)
         {
-            case WeaponType.dagger:
-                while (true)
-                {
-                    coolTime = 360 / attackSpeed;
-                    transform.RotateAround(playerTransform.position, Vector3.forward, coolTime * Time.deltaTime);
-                    await UniTask.Yield();
-                }                
-                //break;
-            case WeaponType.sword:
-                while (true)
-                {
-                    coolTime = 360 / attackSpeed;
-                    transform.RotateAround(playerTransform.position, Vector3.forward, coolTime * Time.deltaTime);
-                    await UniTask.Yield();
-                }
-                //break;
-            case WeaponType.gun:
-                while (true)
-                {
-                    if (enemy == null)
-                        enemy = gameSceneController.GetTargetEnemy(attackRange);
-                    if (enemy != null)
+            switch (_type)
+            {
+                case WeaponType.dagger:
+                    while (true)
                     {
-                        await UniTask.Delay((int)attackSpeed * 1000, cancellationToken: getTargetEnemyCancel);
-                        if (isRightWeapon)
+                        coolTime = 360 / attackSpeed;
+                        transform.RotateAround(playerTransform.position, Vector3.forward, coolTime * Time.deltaTime);
+                        await UniTask.Yield();
+                    }
+                //break;
+                case WeaponType.sword:
+                    while (true)
+                    {
+                        coolTime = 360 / attackSpeed;
+                        transform.RotateAround(playerTransform.position, Vector3.forward, coolTime * Time.deltaTime);
+                        await UniTask.Yield();
+                    }
+                //break;
+                case WeaponType.gun:
+                    while (true)
+                    {
+                        if (enemy == null)
+                            enemy = gameSceneController.GetTargetEnemy(attackRange);
+                        if (enemy != null)
                         {
-                            var direction = enemy.transform.position - transform.position;
-                            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                            await UniTask.Delay((int)attackSpeed * 1000, cancellationToken: getTargetEnemyCancel);
+                            if (isRightWeapon)
+                            {
+                                var direction = enemy.transform.position - transform.position;
+                                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                            }
+                            else
+                            {
+                                var direction = enemy.transform.position - transform.position;
+                                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                                transform.rotation = Quaternion.AngleAxis(angle - 180, Vector3.forward);
+                            }
+                            gameSceneController.FireBullet(enemy, type, _transform);
+                            enemy = null;
                         }
-                        else
-                        {
-                            var direction = enemy.transform.position - transform.position;
-                            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                            transform.rotation = Quaternion.AngleAxis(angle - 180, Vector3.forward);
-                        }                        
-                        gameSceneController.FireBullet(enemy, type, _transform);
-                        enemy = null;
+                        await UniTask.Yield();
                     }
-                    await UniTask.Yield();
-                }
                 //break;
-            case WeaponType.ninjastar:
-                while (true)
-                {
-                    if(enemy == null)
-                        enemy = gameSceneController.GetTargetEnemy(attackRange);
-                    if (enemy != null)
+                case WeaponType.ninjastar:
+                    while (true)
                     {
-                        await UniTask.Delay((int)attackSpeed * 1000, cancellationToken: getTargetEnemyCancel);
-                        gameSceneController.FireBullet(enemy, type, _transform);
-                        enemy = null;
+                        if (enemy == null)
+                            enemy = gameSceneController.GetTargetEnemy(attackRange);
+                        if (enemy != null)
+                        {
+                            await UniTask.Delay((int)attackSpeed * 1000, cancellationToken: getTargetEnemyCancel);
+                            gameSceneController.FireBullet(enemy, type, _transform);
+                            enemy = null;
+                        }
+                        await UniTask.Yield();
                     }
-                    await UniTask.Yield();
-                }
                 //break;
-            default:
-                break;
+                default:
+                    break;
+            }
         }
     }
 }
